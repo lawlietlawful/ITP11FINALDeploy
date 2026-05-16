fab0415cfd8552d849fe9c3fa8ea02d5c3e3a79d
lawliet <lianzyrellelorejo21@gmail.com>
2026-05-16 22:51:14 +0800

    Trust Render Proxies to fix CSRF 419 error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->redirectGuestsTo(fn () => route('login'));
 
         // Register custom role middleware alias
